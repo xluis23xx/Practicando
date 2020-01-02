343727eeac6675984f6713f52ecdc37e11d76fb8
ltirado23 <luis_alexander_23_05_96@hotmail.com>
2020-01-01 21:59:29 -0500

    Finalizacion de crud menu

--- public/assets/pages/scripts/admin/menu/index.js
+++ public/assets/pages/scripts/admin/menu/index.js
@@ -18,7 +18,21 @@ $(document).ready(function () {
 
     $('.eliminar-menu').on('click', function(){
         event.preventDefault();
-    })
+        const url = $(this).attr('href');
+        swal({
+            title: '¿Está seguro que desea eliminar el registro?',
+            text: "Esta acción no se puede deshacer",
+            icon: 'warning',
+            buttons: {
+                cancel: "Cancelar",
+                confirm: "Aceptar"
+            }
+        }).then((value) => {
+            if (value){
+                window.location.href = url;
+            }
+        });
+    });
 
     $('#nestable').nestable('expandAll');
 });
